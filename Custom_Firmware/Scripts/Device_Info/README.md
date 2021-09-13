b03453210038377506f584b4cf4c507d5588c4fe
ludwig-v <ludwig-v@github.com>
2021-09-13 18:42:11 +0200

    Device info script

---
+++ Custom_Firmware/Scripts/Device_Info/README.md
@@ -0,0 +1,13 @@
+### Display device informations
+
+Output example:
+
+    Device UUID: 677f0fb6301769d2xxxxxxxxxxxxxxxx
+    Device Old UUID: 64f5170491de3844564c9a662d99094a
+    Device Manufacturer: GuanSheng
+    Device Version: 255
+    Device Type: U2W
+    Device Custom Type: PU
+    Device Activated Brands: ALFA ROMEO,BMW,AUDI,MERCEDES,CADILLAC,PORSCHE,JAGUAR,LINCOLN,LAND ROVER,MASERATI,VOLVO,ALL_CAR,
+    Software Version: 2021.08.30.0022
+
---
+++ Custom_Firmware/Scripts/Device_Info/U2W.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# turn off red light
+echo 1 >/sys/class/gpio/gpio2/value;
+
+UUID=`cat /sys/fsl_otp/HW_OCOTP_CFG0 /sys/fsl_otp/HW_OCOTP_CFG1 /sys/fsl_otp/HW_OCOTP_MAC0 /sys/fsl_otp/HW_OCOTP_MAC1 | tr -d '\n' | sed 's/0x//g'`
+OLD_UUID=`cat /sys/class/bluetooth/hci0/address /sys/class/net/wlan0/address | md5sum - | awk '{print $1}'`
+FACTORY=`cat /etc/factory_name | tr -d '\n'`
+VERSION=`cat /etc/box_version | tr -d '\n'`
+VERSION_INT=`printf '%d' "'$VERSION"`
+TYPE=`cat /etc/box_product_type | tr -d '\n'`
+CUSTOM_TYPE=`cat /tmp/box_custom_type | tr -d '\n'`
+BRANDS=`cat /etc/support_car_brands | tr -d '\n'`
+SOFT_VERSION=`cat /etc/software_version | tr -d '\n'`
+
+echo "Device UUID: $UUID"
+echo "Device Old UUID: $OLD_UUID"
+echo "Device Manufacturer: $FACTORY"
+echo "Device Version: $VERSION_INT"
+echo "Device Type: $TYPE"
+echo "Device Custom Type: $CUSTOM_TYPE"
+echo "Device Activated Brands: $BRANDS"
+echo "Software Version: $SOFT_VERSION"
+
+# turn on red light
+echo 0 >/sys/class/gpio/gpio2/value;
+
+exit 0
