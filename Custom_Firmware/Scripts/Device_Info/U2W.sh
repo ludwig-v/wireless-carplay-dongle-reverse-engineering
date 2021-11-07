e347cc149c5763e90778cb7a887321dfce10ca60
ludwig-v <ludwig-v@github.com>
2021-11-07 10:42:10 +0100

    Wifi Chip display info

--- Custom_Firmware/Scripts/Device_Info/U2W.sh
+++ Custom_Firmware/Scripts/Device_Info/U2W.sh
@@ -12,6 +12,7 @@ TYPE=`cat /etc/box_product_type | tr -d '\n'`
 CUSTOM_TYPE=`cat /tmp/box_custom_type | tr -d '\n'`
 BRANDS=`cat /etc/support_car_brands | tr -d '\n'`
 SOFT_VERSION=`cat /etc/software_version | tr -d '\n'`
+WIFI_CHIP_ID=`cat /sys/bus/sdio/devices/mmc0:0001:1/device`
 
 echo "Device UUID: $UUID"
 echo "Device Old UUID: $OLD_UUID"
@@ -19,7 +20,14 @@ echo "Device Manufacturer: $FACTORY"
 echo "Device Version: $VERSION_INT"
 echo "Device Type: $TYPE"
 echo "Device Custom Type: $CUSTOM_TYPE"
-echo "Device Activated Brands: $BRANDS"
+if [ "$WIFI_CHIP_ID" == "0xb822" ]; then
+	echo "Device Wifi: Realtek RTL8822BS"
+elif [ "$WIFI_CHIP_ID" == "0x4354" ]; then
+	echo "Device Wifi: Broadcom 4354"
+elif [ "$WIFI_CHIP_ID" == "0x9149" ] || [ "$WIFI_CHIP_ID" == "0x9141" ]; then
+	echo "Device Wifi: Marvell 8987"
+fi
+echo "Software Activated Brands: $BRANDS"
 echo "Software Version: $SOFT_VERSION"
 
 # turn on red light
