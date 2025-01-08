7a14f3857f5e2c6b23382af73a67da1faad74aee
Ludwig <ludwig-v@users.noreply.github.com>
2025-01-08 17:17:28 +0100

    Updated Device Info script to support all Wifi chips

--- Custom_Firmware/Scripts/Device_Info/README.md
+++ Custom_Firmware/Scripts/Device_Info/README.md
@@ -2,12 +2,21 @@
 
 Output example:
 
-    Device UUID: 677f0fb6301769d2xxxxxxxxxxxxxxxx
-    Device Old UUID: 64f5170491de3844564c9a662d99094a
+    ----------------------------------------------------------------
+    Device UUID: 6fb3104d5834ffadae1c377b18c162f8
+    Device Old UUID: 613b461e0595e4e6fbdcaeb76c2b314f
     Device Manufacturer: GuanSheng
-    Device Version: 255
-    Device Type: U2W
-    Device Custom Type: PU
-    Device Activated Brands: ALFA ROMEO,BMW,AUDI,MERCEDES,CADILLAC,PORSCHE,JAGUAR,LINCOLN,LAND ROVER,MASERATI,VOLVO,ALL_CAR,
-    Software Version: 2021.08.30.0022
+    Device Version: 2
+    Device Type: U2AW_AUTOKIT
+    Device Custom Type: 
+    Device Wifi: Realtek RTL8822BS
+    Software Activated Brands: ALFA ROMEO,BMW,AUDI,MERCEDES,CADILLAC,PORSCHE,JAGUAR,LINCOLN,LAND ROVER,MASERATI,VOLVO,ALL_CAR,
+    Software Version: 2023.07.15.1119
+    Activation data (Base64 encoded) - SAVE IT -:
+    PPDATLl6nov0m9ONQ4C2lsx5/MK8ahxUxWFpQcLmUrEdNy+jGcJqb4kqMT/6hEgGQI5IzYqZJMrg
+    To+kMXzfmAF+H9EPK//e71+zLeYU7yM+Ia87Wam2MfpVBBXTIpeTAPdI7UnDnzQrx35xaeRBnyQ1
+    JA9PGfZZEX2JrOyx6367dc5fvRYOr8TAyLFASf2Ihn++YhYxtZPvSGsRM1SqGpxlgyPr0EWMesJq
+    HDztJ+59HKOW5R/iB/HRlCf2QK4W7e5fVW4BhjzTnsaAaUxqtUG4for+w/Y5zXeET2Cymn8H2GwB
+    W5Bzw+uxrZs3JfpTOUNYJ29jN8sDXYpT152hKg==
+    ----------------------------------------------------------------
 
--- Custom_Firmware/Scripts/Device_Info/U2W.sh
+++ Custom_Firmware/Scripts/Device_Info/U2W.sh
@@ -5,15 +5,17 @@ echo 1 >/sys/class/gpio/gpio2/value;
 
 UUID=`cat /sys/fsl_otp/HW_OCOTP_CFG0 /sys/fsl_otp/HW_OCOTP_CFG1 /sys/fsl_otp/HW_OCOTP_MAC0 /sys/fsl_otp/HW_OCOTP_MAC1 | tr -d '\n' | sed 's/0x//g'`
 OLD_UUID=`cat /sys/class/bluetooth/hci0/address /sys/class/net/wlan0/address | md5sum - | awk '{print $1}'`
-FACTORY=`cat /etc/factory_name | tr -d '\n'`
-VERSION=`cat /etc/box_version | tr -d '\n'`
+FACTORY=`cat /etc/factory_name 2>/dev/null | tr -d '\n'`
+VERSION=`cat /etc/box_version 2>/dev/null | tr -d '\n'`
 VERSION_INT=`printf '%d' "'$VERSION"`
 TYPE=`cat /etc/box_product_type | tr -d '\n'`
-CUSTOM_TYPE=`cat /tmp/box_custom_type | tr -d '\n'`
-BRANDS=`cat /etc/support_car_brands | tr -d '\n'`
-SOFT_VERSION=`cat /etc/software_version | tr -d '\n'`
+CUSTOM_TYPE=`cat /tmp/box_custom_type 2>/dev/null | tr -d '\n'`
+BRANDS=`cat /etc/support_car_brands 2>/dev/null | tr -d '\n'`
+SOFT_VERSION=`cat /etc/software_version 2>/dev/null | tr -d '\n'`
 WIFI_CHIP_ID=`cat /sys/bus/sdio/devices/mmc0:0001:1/device`
+ACTIVATION_DATA=`cat /etc/uuid_sign 2>/dev/null | tr -d '\n' | base64`
 
+echo "----------------------------------------------------------------"
 echo "Device UUID: $UUID"
 echo "Device Old UUID: $OLD_UUID"
 echo "Device Manufacturer: $FACTORY"
@@ -22,13 +24,24 @@ echo "Device Type: $TYPE"
 echo "Device Custom Type: $CUSTOM_TYPE"
 if [ "$WIFI_CHIP_ID" == "0xb822" ]; then
 	echo "Device Wifi: Realtek RTL8822BS"
+elif [ "$WIFI_CHIP_ID" == "0xc822" ]; then
+	echo "Device Wifi: Realtek RTL8822BS"
 elif [ "$WIFI_CHIP_ID" == "0x4354" ]; then
 	echo "Device Wifi: Broadcom 4354"
+elif [ "$WIFI_CHIP_ID" == "0x4358" ] || [ "$WIFI_CHIP_ID" == "0xaa31" ]; then
+	echo "Device Wifi: Broadcom 4358"
+elif [ "$WIFI_CHIP_ID" == "0xb733" ]; then
+	echo "Device Wifi: Realtek RTL8733BS"
 elif [ "$WIFI_CHIP_ID" == "0x9149" ] || [ "$WIFI_CHIP_ID" == "0x9141" ]; then
 	echo "Device Wifi: Marvell 8987"
 fi
 echo "Software Activated Brands: $BRANDS"
 echo "Software Version: $SOFT_VERSION"
+echo "Activation data (Base64 encoded) - SAVE IT -:"
+echo "$ACTIVATION_DATA"
+
+echo "----------------------------------------------------------------"
+echo ""
 
 # turn on red light
 echo 0 >/sys/class/gpio/gpio2/value;
